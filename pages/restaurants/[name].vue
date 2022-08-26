f7e5a4912d907e683bf7ebb0a36166bb6d2c3c78
I-Skingley <ijskingley@gmail.com>
2022-08-26 16:55:34 +0200

    Populate restaurant page. Add responsive image

--- pages/restaurants/[name].vue
+++ pages/restaurants/[name].vue
@@ -1,6 +1,84 @@
+<script setup lang="ts">
+//import {} from "#app" //scope to this page solves "name" issue
+import restaurants from "@/data.json";
+
+const route = useRoute();
+const name = route.params.name;
+
+const restaurant = restaurants.find((r) => r.name === name);
+</script>
+
 <template>
   <div>
     <Nav />
-    This is the restaurant data
+    <div class="restaurant-container">
+      <div class="image-container">
+        <!-- Made a change from the course since images didn't scale to half monitor size. 
+        Since we're using bootstrap, used img-fluid class for responsive image and removed .image-container image styling -->
+        <img
+          :src="restaurant.imageUrl"
+          class="img-fluid"
+          alt="Picture of {{ restaurant.name }}"
+        />
+      </div>
+      <div class="info-container">
+        <h1>{{ restaurant.name }}</h1>
+        <div class="stats-container">
+          <h5>Revenue (in billions)</h5>
+          <p>{{ restaurant.revenue }}</p>
+        </div>
+        <div class="stats-container">
+          <h5>Number of Stores</h5>
+          <p>{{ restaurant.numberOfStores }}</p>
+        </div>
+        <p class="content">{{ restaurant.content }}</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.restaurant-container {
+  display: flex;
+}
+.image-container {
+  width: 75%;
+  height: flex;
+}
+
+.restaurant-not-found {
+  height: 75vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.info-container {
+  padding: 3rem;
+  width: 50%;
+}
+.info-container h1 {
+  text-transform: uppercase;
+  font-size: 6rem;
+  margin-bottom: 3rem;
+}
+.stats-container {
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 1rem;
+}
+.stats-container h5 {
+  width: 20rem;
+  font-size: 2rem;
+  margin: 0;
+  margin-right: 5rem;
+}
+.stats-container p {
+  font-size: 2rem;
+  margin: 0;
+}
+.content {
+  font-size: 1.5rem;
+  margin-top: 4rem;
+}
+</style>
